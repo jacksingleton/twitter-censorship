9de6a45e0725601cb9d0b3c5727412dc821af918
Jack Singleton <git@jacksingleton.com>
2014-05-11 21:00:03 -0700

    added back sleep to util.random_wait

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import time
 import random
 
 def random_wait():
-    float(random.randint(0, 1000)) / 1000
+    time.sleep(float(random.randint(0, 1000)) / 1000)
 
 def flatten(list_of_lists):
     return reduce(list.__add__, list_of_lists)
